b282a53973a41b35e51cd9b5ce11f49be33772bf
Cornel Gurin <you@example.comcornelgurin@gmail.com>
2020-10-21 15:44:28 +0200

    second commit

---
+++ src/Cornel/Task_12.java
@@ -0,0 +1,15 @@
+package Cornel;
+
+public class Task_12 {
+
+
+        public static void main(String args[]){
+
+            String [] footPortion = { "1dl milk","1 slice of bread","100gr chicken breast","200gr salad"};
+            int [] kcalPerPortion = {62,74,165,33};
+
+
+
+
+        }
+}
