3dfd20e5c1d0542c58d1ee10fb056278e50dd1e7
Cornel Gurin <you@example.comcornelgurin@gmail.com>
2020-10-21 09:59:14 +0200

    first commit

---
+++ src/Cornel/Tak_03.java
@@ -0,0 +1,13 @@
+package Cornel;
+
+import java.util.Scanner;
+
+public class Tak_03 {
+
+    public static void main(String [] args){
+
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the first number");
+
+    }
+}
