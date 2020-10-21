801fba6a7a07001fc0de440302c6eb8450b1d98f
Scheyk <kathrin.dostal@gmail.com>
2020-10-21 10:08:38 +0200

    finish intermediate one, with commits

--- src/Kathi/IntermediateOne.java
+++ src/Kathi/IntermediateOne.java
@@ -1,8 +1,22 @@
 package Kathi;
 
+/**
+ * Write a program that asks the user for the first number and the last number and
+ * then prints odd numbers between those two.
+ */
+
 public class IntermediateOne {
 
     public static void main(String[]args){
 
+        TheMethods ungerade = new TheMethods();  // <-- call the class with the methods
+        ungerade.loopThree();   // <--- call the function.
+
+
+        /*
+        you find the function in the class TheMethods.
+        start on line 26 and end on line 39.
+        name of the function: loopThree
+         */
     }
 }
--- src/Kathi/TheMethods.java
+++ src/Kathi/TheMethods.java
@@ -1,5 +1,7 @@
 package Kathi;
 
+import java.util.Scanner;
+
 /**
  * this class is only for the functions.
  */
@@ -15,6 +17,22 @@ public class TheMethods {
     public static void loopTwo(){  // <--- for BasicTwo class
         for(int i = 1; i <=100; i++){
             if(i % 2 != 0){
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+
+    public static void loopThree(){    // <--- for IntermediateOne class
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("give me the first number");
+        int one = in.nextInt();
+        System.out.println("now,the second number");
+        int two = in.nextInt();
+
+        for(int i = one; i < two; i++){
+            if(i % 2 == 0){
                 continue;
             }
             System.out.println(i);
